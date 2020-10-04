ef24160bb0318d605a2a170091b9476fd07b045f
Sean Breckenridge <seanbrecke@gmail.com>
2020-10-03 20:15:40 -0700

    mypy: improve typing

--- greasyfork_archive.py
+++ greasyfork_archive.py
@@ -1,12 +1,12 @@
 import sys
 import json
 import time
-from typing import List, Callable, Mapping, Optional
+from typing import List, Callable, Mapping, Optional, Dict, Any
 
 import click
 import requests
-import bs4
-import backoff
+import bs4  # type: ignore[import]
+import backoff  # type: ignore[import]
 
 USER_AGENT = (
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0"
@@ -14,10 +14,10 @@ USER_AGENT = (
 BASE_GREASYFORK_URL = "https://greasyfork.org"
 BASE_GREASYFORK_ENGLISH_URL = f"{BASE_GREASYFORK_URL}/en"
 BASE_USER_URL = f"{BASE_GREASYFORK_ENGLISH_URL}/users"
-get_user_url: Callable = lambda user_id: f"{BASE_USER_URL}/{user_id}"
+get_user_url: Callable[[int], str] = lambda user_id: f"{BASE_USER_URL}/{user_id}"
 
 
-def backoff_hdlr(details):
+def backoff_hdlr(details: Dict[str, Any]) -> None:
     click.echo(
         "Backing off {wait:0.1f} seconds afters {tries} tries "
         "calling function {target} with args {args} and kwargs "
@@ -91,9 +91,9 @@ class UserScript:
             self.script_version: str = self.el["data-script-version"]
             self.sensitive: bool = self.el["data-sensitive"] == "true"
             self.language: str = self.el["data-script-language"]
-            self.css_available_as_js: bool = self.el[
-                "data-css-available-as-js"
-            ] == "true"
+            self.css_available_as_js: bool = (
+                self.el["data-css-available-as-js"] == "true"
+            )
         except KeyError as k:
             click.echo(
                 "Could not find data-attribute '{}' in list element".format(str(k)),
@@ -121,7 +121,7 @@ class UserScript:
     def __str__(self) -> str:
         return self.__repr__()
 
-    def to_dict(self) -> str:
+    def to_dict(self) -> Dict[str, Any]:
         return {k: getattr(self, k) for k in self.__class__.attrs}
 
     @property
@@ -150,14 +150,14 @@ def get_user_scripts(user_url: str) -> List[bs4.element.PageElement]:
     return script_items
 
 
-def main_wrapper(greasyfork_user_id: int, output_file: str):
+def main_wrapper(greasyfork_user_id: int, output_file: str) -> int:
     script_html_elements: List[bs4.element.PageElement] = get_user_scripts(
         get_user_url(greasyfork_user_id)
     )
     script_items: List[UserScript] = list(
         map(lambda el: UserScript(el), script_html_elements)
     )
-    json_dict: Mapping = {
+    json_dict: Dict[str, List[Dict[str, Any]]] = {
         "greasyfork_scripts": list(map(lambda s: s.to_dict(), script_items))
     }
     if output_file:
@@ -176,5 +176,5 @@ def main_wrapper(greasyfork_user_id: int, output_file: str):
     type=click.Path(),
     help="JSON filepath to output scraped data to",
 )
-def main(greasyfork_user_id: int, output_file: str):
+def main(greasyfork_user_id: int, output_file: str) -> None:
     sys.exit(main_wrapper(greasyfork_user_id, output_file))
